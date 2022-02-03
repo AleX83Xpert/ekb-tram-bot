cb4ab9854aa2f3988bd862ec6d509a7cfacbc367
AleX83Xpert <AleX83Xpert@mail.ru>
2022-02-03 15:17:51 +0500

    codestyle for logger

--- logger.js
+++ logger.js
@@ -1,12 +1,12 @@
 const { createLogger, format, transports } = require('winston');
 const { combine, splat, timestamp, printf } = format;
 
-const myFormat = printf( ({ level, message, timestamp , ...metadata}) => {
-  let msg = `${timestamp} [${level}] : ${message} `  
-  if(!!metadata) {
-	const metaStr = JSON.stringify(metadata)
-    if(metaStr !== '{}') {
-        msg += metaStr
+const myFormat = printf(({ level, message, timestamp, ...metadata }) => {
+  let msg = `${timestamp} [${level}] : ${message} `
+  if (!!metadata) {
+    const metaStr = JSON.stringify(metadata)
+    if (metaStr !== '{}') {
+      msg += metaStr
     }
   }
   return msg.trim()
@@ -15,13 +15,14 @@ const myFormat = printf( ({ level, message, timestamp , ...metadata}) => {
 const logger = createLogger({
   level: 'debug',
   format: combine(
-	format.colorize(),
-	splat(),
-	timestamp(),
-	myFormat
+    format.colorize(),
+    splat(),
+    timestamp(),
+    myFormat
   ),
   transports: [
-	new transports.Console(),
+    new transports.Console(),
+    new transports.File({ filename: 'combined.log' }),
   ]
 });
 
