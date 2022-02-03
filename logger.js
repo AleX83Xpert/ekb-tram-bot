4fcfa29431064db5ad07ae921fd874fd4347825a
AleX83Xpert <AleX83Xpert@mail.ru>
2022-02-03 13:31:08 +0500

    logger added

---
+++ logger.js
@@ -0,0 +1,28 @@
+const { createLogger, format, transports } = require('winston');
+const { combine, splat, timestamp, printf } = format;
+
+const myFormat = printf( ({ level, message, timestamp , ...metadata}) => {
+  let msg = `${timestamp} [${level}] : ${message} `  
+  if(!!metadata) {
+	const metaStr = JSON.stringify(metadata)
+    if(metaStr !== '{}') {
+        msg += metaStr
+    }
+  }
+  return msg.trim()
+});
+
+const logger = createLogger({
+  level: 'debug',
+  format: combine(
+	format.colorize(),
+	splat(),
+	timestamp(),
+	myFormat
+  ),
+  transports: [
+	new transports.Console(),
+  ]
+});
+
+module.exports = logger
