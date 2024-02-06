472f84172202c3e80effc0373c76ce37a49d6fa5
AleX83Xpert <AleX83Xpert@mail.ru>
2024-02-06 09:46:20 +0500

    move to the pino logger set minimum node version to 20.11.0

--- logger.js
+++ logger.js
@@ -1,29 +1,8 @@
-const { createLogger, format, transports } = require('winston');
-const { combine, splat, timestamp, printf } = format;
+const pino = require('pino')
 
-const myFormat = printf(({ level, message, timestamp, ...metadata }) => {
-  let msg = `${timestamp} [${level}] : ${message} `
-  if (!!metadata) {
-    const metaStr = JSON.stringify(metadata)
-    if (metaStr !== '{}') {
-      msg += metaStr
-    }
-  }
-  return msg.trim()
-});
-
-const logger = createLogger({
-  level: 'debug',
-  format: combine(
-    format.colorize(),
-    splat(),
-    timestamp(),
-    myFormat
-  ),
-  transports: [
-    new transports.Console(),
-    new transports.File({ filename: 'combined.log' }),
-  ]
-});
+const logger = pino({
+  name: 'bot',
+  timestamp: () => `,"time":"${new Date(Date.now()).toISOString()}"`,
+})
 
 module.exports = logger
